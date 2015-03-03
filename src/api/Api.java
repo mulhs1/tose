570a2866e5dc845bb3fbc29e3c081ff0c186068e
Pascal Graf <Pascal Graf@147.87.142.68>
2015-03-03 09:47:35 +0100

    Maven install and Project configuration

---
+++ src/api/Api.java
@@ -0,0 +1,9 @@
+package api;
+
+public class Api {
+
+	public static String getSearchResults(String searchArgument) {
+		return "Test";
+	}
+	
+}
---
+++ src/tose/gui/Main.java
@@ -0,0 +1,15 @@
+package tose.gui;
+
+import api.Api;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Api.getSearchResults("Test"));
+
+		
+		 
+	}
+
+}
