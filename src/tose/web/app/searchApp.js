5683e88e7052daa202acad5dcde4f6af4ce7b9c4
Doomhower <rutid1@bfh.ch>
2015-03-09 16:41:28 +0100

    Correction of src links for skripts

--- src/tose/web/app/searchApp.js
+++ src/tose/web/app/searchApp.js
@@ -1,6 +1,6 @@
 angular.module('flickrSearch', [])
   .controller('stageOneCtrl', function ($scope, $http) {
-    $http.get('dummy_pictures.json').success(function(dummy_pictures) {
+    $http.get('app/dummy_pictures.json').success(function(dummy_pictures) {
       $scope.dummy_pictures = dummy_pictures;
     });
   });
