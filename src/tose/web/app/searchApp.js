e1297e1435b86c310c2a6d482592647378c007e3
Patrick Mutzner <patrick@mutzner.be>
2015-03-14 08:29:38 +0100

    bvjkdfh

--- src/tose/web/app/searchApp.js
+++ src/tose/web/app/searchApp.js
@@ -1,7 +1,7 @@
 angular.module('flickrSearch', [])
   .controller('stageOneCtrl', function ($scope, $http) {
-    $http.get('app/dummy_pictures.json').success(function(dummy_pictures) {
-      $scope.dummy_pictures = dummy_pictures;
+    $http.get('https://tose.mimacom.com/ProFosService/service/searchMetadataService?query=test').success(function(imgdata) {
+      $scope.imgdata = imgdata;
     });
   });
   
