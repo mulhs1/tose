3277fdfcc0d62e4e2deeca53845d80666bf1e026
Doomhower <rutid1@bfh.ch>
2015-03-14 08:38:42 +0100

    Check

--- src/tose/web/app/searchApp.js
+++ src/tose/web/app/searchApp.js
@@ -2,6 +2,7 @@ angular.module('flickrSearch', [])
   .controller('stageOneCtrl', function ($scope, $http) {
     $http.get('https://tose.mimacom.com/ProFosService/service/searchMetadataService?query=test').success(function(imgdata) {
       $scope.imgdata = imgdata;
+      console.log(imgdata);
     });
   });
   
