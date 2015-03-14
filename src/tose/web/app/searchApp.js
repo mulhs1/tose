7d83e90bf4daf2665a519f367b3ccf25f6fbf404
Patrick Mutzner <patrick@mutzner.be>
2015-03-14 08:06:29 +0100

    1. Anpassung auf neuen Web Service

--- src/tose/web/app/searchApp.js
+++ src/tose/web/app/searchApp.js
@@ -4,3 +4,10 @@ angular.module('flickrSearch', [])
       $scope.dummy_pictures = dummy_pictures;
     });
   });
+  
+  function WebServiceCall($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        success(function(data) {
+            $scope.imgdata = data;
+        });
+}
