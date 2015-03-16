4452acb61b46551d834bb763461485b9a6b8fe08
Patrick Mutzner <patrick@mutzner.be>
2015-03-16 14:29:18 +0100

    Implemented Pass-Through and processing of query between Sites and build query to api

--- src/tose/web/app/searchApp.js
+++ src/tose/web/app/searchApp.js
@@ -1,7 +1,16 @@
 angular.module('flickrSearch', [])
   .controller('stageOneCtrl', function ($scope, $http) {
-    $http.get('https://tose.mimacom.com/ProFosService/service/searchMetadataService?query=test').success(function(imgdata) {
-      $scope.imgdata = imgdata;
-      console.log(imgdata);
-    });
+    $scope.init = function(query){
+		$scope.query = query.split(' ').join('+');
+		$scope.searchurl = 'https://tose.mimacom.com/ProFosService/service/searchMetadataService?query='+$scope.query;
+		
+		$http.get($scope.searchurl).success(function(imgdata) {
+			$scope.imgdata = imgdata;
+			console.log(imgdata);
+		});
+	}
   });
+  
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
